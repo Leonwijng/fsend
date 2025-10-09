dcaf1a198d019a4525547be308e6d53df9987b55
scott-mescudi <anish.chittu2007@gmail.com>
2025-10-09 17:25:52 +0200

    lj ljlk;hi;hblihblhblhby

--- client/main.go
+++ client/main.go
@@ -1,7 +1,101 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	putfile uint8 = iota
+	listFiles
+	streamFile
+	ping
+	bye
+)
+
+func putFile(filePath string, conn net.Conn, bufSize uint32) (err error) {
+	fn, err := os.Stat(filePath)
+	if err != nil {
+		return
+	}
+
+	if fn.IsDir() {
+		return fmt.Errorf("error: %s is a directory", filePath)
+	}
+
+	var (
+		fname     = fn.Name()
+		fnameSize = len(fname)
+		fsize     = fn.Size()
+	)
+
+	if fnameSize > 256 {
+		return fmt.Errorf("filename is to large, must be smaller than 256 characters")
+	}
+
+	err = binary.Write(conn, binary.LittleEndian, putfile)
+	if err != nil {
+		return
+	}
+
+	err = binary.Write(conn, binary.LittleEndian, uint8(fnameSize))
+	if err != nil {
+		return
+	}
+
+	_, err = conn.Write([]byte(fname))
+	if err != nil {
+		return
+	}
+
+	err = binary.Write(conn, binary.LittleEndian, uint64(fsize))
+	if err != nil {
+		return
+	}
+
+	err = binary.Write(conn, binary.LittleEndian, bufSize)
+	if err != nil {
+		return
+	}
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	if bufSize == 0 {
+		bufSize = 32 * 1024
+	}
+	buf := make([]byte, bufSize)
+	
+
+	writer := bufio.NewWriter(conn)
+	_, err = io.CopyBuffer(writer, f, buf)
+	if err != nil {
+		return err
+	}
+
+	
+	err = writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-	fmt.Println("hi huzz")
+	l, err := net.Dial("tcp", ":3002")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer l.Close()
+
+	putFile("mascott.png", l, 1024)
 }
--- server/main.go
+++ server/main.go
@@ -1,23 +1,122 @@
 package main
 
 import (
+	"encoding/binary"
+	"errors"
 	"fmt"
-	"log"
+	"io"
 	"net"
+	"os"
+	"sync"
 )
 
 const (
 	putfile uint8 = iota
 	listFiles
 	streamFile
+	ping
+	bye
 )
 
-func main() {
-	lis, err := net.Listen("tcp", ":3001")
+type ServerContext struct {
+	clients map[net.Conn]struct{}
+	lis     net.Listener
+	mu      sync.Mutex
+}
+
+func putFile(conn net.Conn) {
+	var (
+		fnameSize uint8
+		fname     string
+
+		fsize   uint64
+		bufSize uint32
+	)
+
+	err := binary.Read(conn, binary.LittleEndian, &fnameSize)
 	if err != nil {
-		log.Fatalln(err)
+		return
 	}
-	defer lis.Close()
+
+	var fnameBytes = make([]byte, fnameSize)
+	_, err = conn.Read(fnameBytes)
+	if err != nil {
+		return
+	}
+	fname = string(fnameBytes)
+
+	f, err := os.Create(fname)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+
+	err = binary.Read(conn, binary.LittleEndian, &fsize)
+	if err != nil {
+		return
+	}
+
+	err = binary.Read(conn, binary.LittleEndian, &bufSize)
+	if err != nil {
+		return
+	}
+
+	if bufSize == 0 {
+		bufSize = 32 * 1024
+	}
+
+	buf := make([]byte, int(bufSize))
+	lr := io.LimitReader(conn, int64(fsize))
+	_, err = io.CopyBuffer(f, lr, buf)
+	if err != nil {
+		if errors.Is(err, io.EOF) {
+			return
+		}
+
+		fmt.Println("copy error:", err)
+		return
+	}
+}
+
+func (s *ServerContext) handleClient(conn net.Conn) {
+	s.mu.Lock()
+	s.clients[conn] = struct{}{}
+	s.mu.Unlock()
+
+	for {
+		var o uint8
+		err := binary.Read(conn, binary.LittleEndian, &o)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		switch o {
+		case putfile:
+			putFile(conn)
+		case listFiles:
+		case streamFile:
+		case ping:
+			_, err = conn.Write([]byte("pong"))
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		case bye:
+			conn.Close()
+			s.mu.Lock()
+			delete(s.clients, conn)
+			s.mu.Unlock()
+		}
+	}
+
+}
+
+func (s *ServerContext) Listen(address string) (err error) {
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
 
 	for {
 		conn, err := lis.Accept()
@@ -26,6 +125,15 @@ func main() {
 			continue
 		}
 
-
+		go s.handleClient(conn)
 	}
+}
+
+func main() {
+	ctx := ServerContext{
+		clients: make(map[net.Conn]struct{}),
+	}
+
+	fmt.Println("listening on :3002 ")
+	panic(ctx.Listen(":3002"))
 }
